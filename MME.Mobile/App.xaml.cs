202808a473eb344f8ac75758db955da2b9c44c11
Sachin Jawale <sachin.jawale@gmail.com>
2023-07-14 18:35:27 +0530

    Add project files.

---
+++ MME.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MME.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
