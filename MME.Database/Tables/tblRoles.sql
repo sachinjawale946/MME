15418ad4ebbdd20230aa14b9914c6cb0e120eea9
Sachin Jawale <sachin.jawale@gmail.com>
2023-07-19 00:32:50 +0530

    roles and users table script added,

---
+++ MME.Database/Tables/tblRoles.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblRoles]
+(
+	[RoleId] INT NOT NULL IDENTITY, 
+	[Role] VARCHAR(50),
+	[Description] VARCHAR(100),
+	[IsActive] BIT,
+    CONSTRAINT [PK_tblRoles] PRIMARY KEY ([RoleId]) 
+)
