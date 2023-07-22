162e5ded88dc89a9ef19b4907764b04ff5b11cdd
Sachin Jawale <sachin.jawale@gmail.com>
2023-07-22 17:19:25 +0530

    post deployment script issue resolved.

--- MME.Database/Scripts/Script.States.sql
+++ MME.Database/Scripts/Script.States.sql
@@ -9,6 +9,25 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+-- ROLES
+INSERT INTO tblRoles
+(Role, Description,DisplayOrder, IsActive)
+VALUES
+('Admin','Full Access To Platform',1,1)
+
+INSERT INTO tblRoles
+(Role, Description, DisplayOrder, IsActive)
+VALUES
+('Contributor','Access To Events Creation/View, Members',2,1)
+
+INSERT INTO tblRoles
+(Role, Description, DisplayOrder, IsActive)
+VALUES
+('Member','Access To Events View, Members',3,1)
+-- ROLES
+
+-- STATES
 INSERT INTO tblStates
 (State,DisplayOrder,IsActive)
 VALUES
@@ -18,3 +37,172 @@ INSERT INTO tblStates
 (State,DisplayOrder,IsActive)
 VALUES
 ('Gujrat',2,1)
+-- STATES
+
+-- PINCODES
+INSERT INTO tblPinCodes
+(PinCode,State,DisplayOrder,IsActive)
+VALUES
+('400071',1,1,1)
+
+INSERT INTO tblPinCodes
+(PinCode,State,DisplayOrder,IsActive)
+VALUES
+('400024',1,2,1)
+
+INSERT INTO tblPinCodes
+(PinCode,State,DisplayOrder,IsActive)
+VALUES
+('400080',2,3,1)
+
+INSERT INTO tblPinCodes
+(PinCode,State,DisplayOrder,IsActive)
+VALUES
+('400604',2,4,1)
+
+INSERT INTO tblPinCodes
+(PinCode,State,DisplayOrder,IsActive)
+VALUES
+('400608',2,5,1)
+
+INSERT INTO tblPinCodes
+(PinCode,State,DisplayOrder,IsActive)
+VALUES
+('400612',2,6,1)
+-- PINCODES
+
+-- CASTES
+INSERT INTO tblCastes
+(Caste,DisplayOrder,IsActive)
+VALUES 
+('General',1,1)
+
+INSERT INTO tblCastes
+(Caste,DisplayOrder,IsActive)
+VALUES 
+('OBC',2,1)
+
+INSERT INTO tblCastes
+(Caste,DisplayOrder,IsActive)
+VALUES 
+('SC',3,1)
+
+INSERT INTO tblCastes
+(Caste,DisplayOrder,IsActive)
+VALUES 
+('ST',4,1)
+-- CASTES
+
+-- EVENT TYPES
+INSERT INTO tblEventTypes
+(EventType,DisplayOrder,IsActive)
+VALUES
+('General',1,1)
+
+INSERT INTO tblEventTypes
+(EventType,DisplayOrder,IsActive)
+VALUES
+('Participation',2,1)
+
+INSERT INTO tblEventTypes
+(EventType,DisplayOrder,IsActive)
+VALUES
+('Fundraiser',3,1)
+
+INSERT INTO tblEventTypes
+(EventType,DisplayOrder,IsActive)
+VALUES
+('Charity',4,1)
+
+INSERT INTO tblEventTypes
+(EventType,DisplayOrder,IsActive)
+VALUES
+('Conferences',5,1)
+-- EVENT TYPES
+
+-- LANGUAGES
+INSERT INTO tblLanguages
+(Language,DisplayOrder, IsActive)
+VALUES
+('English',1,1)
+
+INSERT INTO tblLanguages
+(Language,DisplayOrder,IsActive)
+VALUES
+('Hindi',2,1)
+
+INSERT INTO tblLanguages
+(Language,DisplayOrder,IsActive)
+VALUES
+('Marathi',3,1)
+
+INSERT INTO tblLanguages
+(Language,DisplayOrder,IsActive)
+VALUES
+('Gujrati',4,1)
+-- LANGUAGES
+
+-- OCCUPATIONS
+INSERT INTO tblOccupations
+(Occupation, DisplayOrder,IsActive)
+VALUES
+('Accountant',1,1)
+
+INSERT INTO tblOccupations
+(Occupation, DisplayOrder,IsActive)
+VALUES
+('Business - Self Employed ',2,1)
+
+INSERT INTO tblOccupations
+(Occupation, DisplayOrder,IsActive)
+VALUES
+('Chartered Accountant',3,1)
+
+INSERT INTO tblOccupations
+(Occupation, DisplayOrder,IsActive)
+VALUES
+('Doctor',4,1)
+
+INSERT INTO tblOccupations
+(Occupation, DisplayOrder,IsActive)
+VALUES
+('Lawyer',5,1)
+
+INSERT INTO tblOccupations
+(Occupation, DisplayOrder,IsActive)
+VALUES
+('Software Engineer',6,1)
+-- OCCUPATIONS
+
+-- RELIGIOUS
+INSERT INTO tblReligions
+(Religion,DisplayOrder,IsActive)
+VALUES
+('Hindu',1,1)
+
+INSERT INTO tblReligions
+(Religion,DisplayOrder,IsActive)
+VALUES
+('Muslim',2,1)
+
+INSERT INTO tblReligions
+(Religion,DisplayOrder,IsActive)
+VALUES
+('Christian',3,1)
+
+INSERT INTO tblReligions
+(Religion,DisplayOrder,IsActive)
+VALUES
+('Sikh',4,1)
+
+
+INSERT INTO tblReligions
+(Religion,DisplayOrder,IsActive)
+VALUES
+('Buddhisht',5,1)
+
+INSERT INTO tblReligions
+(Religion,DisplayOrder,IsActive)
+VALUES
+('Other',6,1)
+-- RELIGIOUS
--- MME.Database/Tables/tblSubCastes.sql
+++ MME.Database/Tables/tblSubCastes.sql
@@ -2,9 +2,7 @@
 (
 	[SubCasteId] INT NOT NULL IDENTITY, 
 	[SubCaste] NVARCHAR(100),
-	[Caste] INT NOT NULL,
 	[DisplayOrder] INT NOT NULL,
 	[IsActive] BIT,
 	CONSTRAINT [PK_tblSubCastes] PRIMARY KEY ([SubCasteId]), 
-    CONSTRAINT [FK_tblSubCastes_tblCastes] FOREIGN KEY ([Caste]) REFERENCES [tblCastes]([CasteId]),
 )
