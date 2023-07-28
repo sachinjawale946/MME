5e81577d24248af7b4be3385bc2798b6d6196943
Sachin Jawale <sachin.jawale@gmail.com>
2023-07-28 18:56:55 +0530

    menu highlight issues fixed on static and dynamic both the parts.

---
+++ MME.Web/Controllers/MembersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MME.Web.Controllers
+{
+    [Authorize(Policy = "MMECookieScheme")]
+    public class MembersController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
