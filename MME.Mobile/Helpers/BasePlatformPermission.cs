bc343e79886da15f31ac96c1badd741aa5ec5a11
Sachin Jawale <sachin.jawale@gmail.com>
2023-08-08 09:18:06 +0530

    Notification permission added in main activity.

---
+++ MME.Mobile/Helpers/BasePlatformPermission.cs
@@ -0,0 +1,17 @@
+﻿using Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace MME.Mobile.Helpers
+{
+    public class PostNotifications : BasePlatformPermission
+    {
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
+                    new (string, bool)[] { (Manifest.Permission.PostNotifications, true) };
+    }
+
+}
--- MME.Mobile/Platforms/Android/MainActivity.cs
+++ MME.Mobile/Platforms/Android/MainActivity.cs
@@ -1,7 +1,12 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
 using Plugin.Firebase.CloudMessaging;
 
 namespace MME.Mobile
@@ -14,7 +19,67 @@ namespace MME.Mobile
             base.OnCreate(savedInstanceState);
             HandleIntent(Intent);
             CreateNotificationChannelIfNeeded();
+            if (!(CheckPermissionGranted(Manifest.Permission.PostNotifications)))
+            {
+                RequestPostNotificationsPermission();
+            }
         }
+
+        public bool CheckPermissionGranted(string Permissions)
+        {
+            // Check if the permission is already available.
+            if (ActivityCompat.CheckSelfPermission(this, Permissions) != Permission.Granted)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static int REQUEST_P0STNOTIFICATIONS = 10023;
+        string[] reqestPermission = { "android.permission.POST_NOTIFICATIONS" };
+        private void RequestPostNotificationsPermission()
+        {
+            if (ActivityCompat.ShouldShowRequestPermissionRationale(this, "android.permission.POST_NOTIFICATIONS"))
+            {
+                // Provide an additional rationale to the user if the permission was not granted
+                // and the user would benefit from additional context for the use of the permission.
+                // For example if the user has previously denied the permission.
+                ActivityCompat.RequestPermissions(this, reqestPermission, REQUEST_P0STNOTIFICATIONS);
+            }
+            else
+            {
+                //P0STNOTIFICATIONS permission has not been granted yet. Request it directly.
+                ActivityCompat.RequestPermissions(this, reqestPermission, REQUEST_P0STNOTIFICATIONS);
+            }
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults); if (requestCode == REQUEST_P0STNOTIFICATIONS)
+            {
+                if (grantResults.Length <= 0)
+                {
+                    // If user interaction was interrupted, the permission request is cancelled and you 
+                    // receive empty arrays.
+                    // Log.Info("error", "User interaction was cancelled.");
+                }
+                else if (grantResults[0] == PermissionChecker.PermissionGranted)
+                {
+                    // Permission was granted.
+                }
+                else
+                {
+                    // Permission denied.
+                    Toast.MakeText(this, " REQUEST_P0STNOTIFICATIONS Permission Denied", ToastLength.Long).Show();
+                }
+            }
+        }
+
 
         protected override void OnNewIntent(Intent intent)
         {
